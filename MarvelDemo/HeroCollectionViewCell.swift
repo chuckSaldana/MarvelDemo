842ebaba468fb3e43304267aaa3dba011a519836
Carlos Saldana <saldanatic@gmail.com>
2019-08-18 11:54:09 -0500

    iPhone simple colletion view and cell.

---
+++ MarvelDemo/HeroCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HeroCollectionViewCell.swift
+//  MarvelDemo
+//
+//  Created by Apocalapsus on 18/08/19.
+//  Copyright © 2019 Carlos. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HeroCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var itemImage: UIImageView!
+    @IBOutlet weak var itemLabel: UILabel!
+}
--- MarvelDemo/ViewController.swift
+++ MarvelDemo/ViewController.swift
@@ -8,7 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "heroCellID"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! HeroCollectionViewCell
+        cell.itemLabel.text = "Test Name"
+        cell.itemImage.image = UIImage(named: "TestImage")
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
