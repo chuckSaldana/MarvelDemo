49f2995d0ec17397d717d1d06fad1dca924f683b
Carlos Saldana <saldanatic@gmail.com>
2019-08-18 10:38:20 -0500

    Initial Commit

---
+++ MarvelDemoTests/MarvelDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  MarvelDemoTests.swift
+//  MarvelDemoTests
+//
+//  Created by Apocalapsus on 18/08/19.
+//  Copyright © 2019 Carlos. All rights reserved.
+//
+
+import XCTest
+@testable import MarvelDemo
+
+class MarvelDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
